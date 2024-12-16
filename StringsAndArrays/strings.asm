c3153d74180d373a13bbf3df11a04477a2c54a3e
steve <stevegranet@protonmail.com>
2024-12-15 21:48:11 -0500

    added store and load examples

--- StringsAndArrays/strings.asm
+++ StringsAndArrays/strings.asm
@@ -28,6 +28,20 @@ DumpRegs        PROTO
 
         MessageEqual BYTE "They are equal!", 0
         MessageNotEqual BYTE "They are not equal", 0
+        MessageStringMove BYTE "===Moving Strings===", 0
+        MessageStringComparison BYTE "===Comparison Strings===", 0
+        MessageStringScanning BYTE "===Scanning Strings===",0
+        MessageStringSto BYTE "===Storing Strings===",0
+        MessageStringsLod BYTE "===Loading Strings===",0
+        
+        MessageNotFound BYTE "The character was not found",0
+        MessageFound BYTE "The character was found at location: ",0
+
+        COUNT = 10
+        emptyString BYTE COUNT DUP(?)
+
+        lodArray DWORD 1,2,3,4,5,6,7,8,9,10
+        lodMultiplier DWORD 10
 .code
 main PROC
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -78,6 +92,10 @@ main PROC
 
 
 ; Move string instructions: MOVSB, MOVSW, and MOVSD
+        mov         edx, OFFSET MessageStringMove
+        call        WriteString
+        call        Crlf
+
         cld                                             ; clear the direction flag (more on this later)
         mov         esi, OFFSET firstString
         mov         edi, OFFSET secondString
@@ -112,7 +130,11 @@ main PROC
         ; |             Set (STD)               |       Decremented         |       High-low        |
         ; -------------------------------------------------------------------------------------------
 
-; Compare string instructions: CMPSB CMPSW CMPSD
+; Compare string instructions: CMPSB CMPSW CMPSD 
+        mov         edx, OFFSET MessageStringComparison
+        call        WriteString
+        call        Crlf
+
         ; test when two strings are equal
         mov         esi, OFFSET thirdString
         mov         edi, OFFSET fourthString
@@ -131,14 +153,114 @@ J2:     call        Crlf
         mov         edi, OFFSET fifthString
         mov         ecx, LENGTHOF fifthString
         cld
-        repe        cmpsb
+        repe        cmpsb                               ; repe is rep while equal, when something is not equal or ecx =0, it exits the loop
         jne         J3                                  ; I can also use jne instead of jnz for clarity
         mov         edx, OFFSET MessageEqual
         call        WriteString
         jmp         J4
 J3:     mov         edx, OFFSET MessageNotEqual
         call        WriteString
-J4:
+J4:     call        Crlf
+        call        Crlf
+; SCASB SCASW SCASD
+;   Compare a value in AL/AX/EAX to a byte, word, or doubleword, respectively, addressed by EDI. 
+;   The instructions are useful when looking for a single value in a string or array. 
+        mov         edx, OFFSET MessageStringScanning
+        call        WriteString
+        call        Crlf
+ 
+        ; finding character
+        mov         edi, OFFSET firstString
+        mov         al, "o"
+        mov         ecx, LENGTHOF firstString
+
+        cld
+        repne       scasb                               ; repeat while not equal
+        jnz         J5                                  ; jump if not found
+        mov         edx, OFFSET MessageFound
+        call        WriteString
+        ; This is what I came up with to find the proper index of the item
+        mov         eax, LENGTHOF firstString
+        sub         eax, ecx
+        call        WriteInt
+        call        Crlf
+
+        ; This looks like another way to find the index, 0 based
+        ; What does this actually do? 
+        ; original: 0004
+        ; edi is incremented until it finds the correct place
+        ; 0004, 0005, 0006, 0007 until it finds it, lets say its at 0007
+        ; we want to know the index so we do 0004-0007
+        ; this gives us -3, then we not it and get 3
+        ; it sounds like we should subtract eax FROM edi (i did, next block example)
+        mov         eax, OFFSET firstString             ; move the memory location into eax of the original string
+        sub         eax, edi                            ; subtract the edi memory address from the original
+        not         eax                                 
+        call        WriteInt
+
+        ; yes this works too but we must dec edi like the book asked to get to 0 based index
+        call        Crlf
+        dec         edi                                 ; dec because the NEXT index is pointed toat by edi when repe finishes
+        mov         eax, edi
+        sub         eax, OFFSET firstString
+        call        WriteInt
+
+        jmp         J6
+J5:     mov         edx, OFFSET MessageNotFound
+        call        WriteString
+J6:     call        Crlf
+
+; STOSB STOSW STOSD
+;   contents of AL/AX/EAX, respectively, in memory at the offset pointed to by EDI
+;   When used with the REP prefix, these instructions are useful for filling all elements of a string or array with a single value
+        call        Crlf
+        mov         edx, OFFSET MessageStringSto
+        call        WriteString
+        call        Crlf
+        mov         eax, 0
+        mov         al, 'a'
+        mov         edi, OFFSET emptyString
+        mov         ecx, COUNT
+        cld         ; set the direction flag to have rep increment
+        rep         stosb
+        mov         edx, OFFSET emptyString
+        call        WriteString
+        call        Crlf
+
+; LODSB LODSW LODSD
+;    load a byte or word from memory at ESI into AL/AX/EAX, respectively
+;    The REP prefix is rarely used with LODS because each new value loaded into the
+;    accumulator overwrites its previous contents. Instead, LODS is used to load a single value
+;
+; In the next example, LODSB substitutes for the following two instructions
+;       mov al,[esi] ; move byte into AL
+;       inc esi ; point to next byte
+        cld
+        call        Crlf
+        mov         edx, OFFSET MessageStringsLod
+        call        WriteString
+        call        Crlf
+
+        mov         esi, OFFSET lodArray                ; load this each esi into eax
+        mov         edi, esi                            ; when storing, store from eax into edi (or the same lodArray index)
+        mov         ecx, LENGTHOF lodArray              ; used for L1 loop, since we aren't using any reps, this is fine
+
+L1:     lodsd                                           ; load esi into eax (eax because lodsD the d means dword)
+        mul         lodMultiplier                       ; multiply eax by lodMultiplier (10)
+        stosd                                           ; store eax into edi (same element as esi)
+        loop        L1                                  ; keep going until there are no more elements
+
+        ; print the results
+        mov         esi, OFFSET lodArray
+        mov         ecx, LENGTHOF lodArray
+L2:     mov         eax, [esi]
+        call        WriteInt
+        mov         al, 2Ch                             ; print a comma
+        call        WriteChar
+        mov         al, 20h                             ; print a space
+        add         esi, TYPE DWORD
+        loop        L2
+
         Invoke      ExitProcess,0
 main ENDP
 END main
